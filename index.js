8e1bcc9694835adaaeedb017e7a0a2745aa9f047
Abagail Hudson <abagailanne@gmail.com>
2019-03-27 20:45:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,18 +13,18 @@ var animal = 'cat'// How can we make sure that this function
 }
 
 function add2(n) {
-  const two = 2
   return n + two
 
   // Feel free to move things around!
-
+  const two = 2
 }
 
-function funkyFunction() {
-  return "FUNKY!"
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
-theFunk
